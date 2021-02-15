ba2c85e4615cadb43ddddf1634bf48d6455aa46a
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-15 16:33:53 -0500

    randomize the string of output password

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+password_list = []
 
 for char in range(1, nr_letters + 1):
-  password += random.choice(letters)
+  password_list.append(random.choice(letters))
 
 for char in range(1, nr_numbers + 1):
-  password += random.choice(numbers)
+  password_list.append(random.choice(numbers))
   
 for char in range(1, nr_symbols + 1):
-  password += random.choice(symbols)
+  password_list.append(random.choice(symbols))
 
-print(password)
+random.shuffle(password_list)
+
+password = ""
+
+for char in password_list:
+  password += char
+
+print(f"Your new password is: {password}")
